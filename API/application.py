8f98a544dcd0f0f3181a68615f5ea88c057a9f10
Miguel Correia <migcid25@gmail.com>
2025-06-24 01:10:25 +0000

    new commit

--- API/application.py
+++ API/application.py
@@ -1,14 +1,18 @@
+from datetime import timedelta
 from flask import Flask
 app = Flask(__name__)
 from flask_sqlalchemy import SQLAlchemy
 
+app.secret_key = 'hello'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.permanent_session_lifetime = timedelta(days=1)
 db = SQLAlchemy(app)
 
 class Drink(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name=db.Column(db.String(80), unique=True, nullable=False)
-    description = db.column(db.String(120))
+    description = db.Column(db.String(120))
     
     def __repr__(self):
         return f"{self.name} - {self.description}"
